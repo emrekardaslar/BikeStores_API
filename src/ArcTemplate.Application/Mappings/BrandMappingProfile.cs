04d3b477ac529e8f3bc42ef82b584f967533c5d8
emrekardaslar <emre.kardaslar@gmail.com>
2024-09-17 21:00:02 +0300

    add BrandMappingProfile

---
+++ src/ArcTemplate.Application/Mappings/BrandMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using ArcTemplate.Application.UseCases.GetBrand;
+using ArcTemplate.Core.Entities;
+using AutoMapper;
+
+
+namespace ArcTemplate.Application.Mappings
+{
+    public class BrandMappingProfile : Profile
+    {
+        public BrandMappingProfile() { 
+            CreateMap<Brand, GetBrandResponse>();
+        }
+    }
+}
--- src/ArcTemplate.Application/UseCases/GetBrand/GetBrandHandler.cs
+++ src/ArcTemplate.Application/UseCases/GetBrand/GetBrandHandler.cs
@@ -2,22 +2,26 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using ArcTemplate.Core.Interfaces;
+using AutoMapper;
 
 namespace ArcTemplate.Application.UseCases.GetBrand
 {
     public class GetBrandHandler : IRequestHandler<GetBrandQuery, GetBrandResponse>
     {
         private readonly IBrandRepository _BrandRepository;
+        private readonly IMapper _mapper;
 
-        public GetBrandHandler(IBrandRepository BrandRepository)
+        public GetBrandHandler(IBrandRepository BrandRepository, IMapper mapper)
         {
             _BrandRepository = BrandRepository;
+            _mapper = mapper;
         }
 
         public Task<GetBrandResponse> Handle(GetBrandQuery request, CancellationToken cancellationToken)
         {
             var Brand = _BrandRepository.GetBrandById(request.Id);
-            return Task.FromResult(new GetBrandResponse { Id = Brand.Id, Name = Brand.Name });
+            var response = _mapper.Map<GetBrandResponse>(Brand);
+            return Task.FromResult(response);
         }
     }
 }
