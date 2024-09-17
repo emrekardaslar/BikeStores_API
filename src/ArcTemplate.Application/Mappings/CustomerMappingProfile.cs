37c2266a8a2da7a13fe9619e986a382f64142196
emrekardaslar <emre.kardaslar@gmail.com>
2024-09-17 21:13:26 +0300

    add CustomerMappingProfile

---
+++ src/ArcTemplate.Application/Mappings/CustomerMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using ArcTemplate.Application.UseCases.GetCustomer;
+using ArcTemplate.Core.Entities;
+using AutoMapper;
+
+namespace ArcTemplate.Application.Mappings
+{
+    public class CustomerMappingProfile : Profile
+    {
+        public CustomerMappingProfile() {
+            CreateMap<Customer, GetCustomerResponse>();
+        }
+    }
+}
--- src/ArcTemplate.Application/UseCases/GetCustomer/GetCustomerHandler.cs
+++ src/ArcTemplate.Application/UseCases/GetCustomer/GetCustomerHandler.cs
@@ -2,16 +2,19 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using ArcTemplate.Core.Interfaces;
+using AutoMapper;
 
 namespace ArcTemplate.Application.UseCases.GetCustomer
 {
     public class GetCustomerHandler : IRequestHandler<GetCustomerQuery, GetCustomerResponse>
     {
         private readonly ICustomerRepository _customerRepository;
+        private readonly IMapper _mapper;
 
-        public GetCustomerHandler(ICustomerRepository customerRepository)
+        public GetCustomerHandler(ICustomerRepository customerRepository, IMapper mapper)
         {
             _customerRepository = customerRepository;
+            _mapper = mapper;
         }
 
         public async Task<GetCustomerResponse> Handle(GetCustomerQuery request, CancellationToken cancellationToken)
@@ -22,12 +25,7 @@ namespace ArcTemplate.Application.UseCases.GetCustomer
                 throw new KeyNotFoundException($"Customer with ID {request.Id} not found.");
             }
 
-            return new GetCustomerResponse
-            {
-                Id = customer.Id,
-                Name = customer.Name,
-                Email = customer.Email
-            };
+            return _mapper.Map<GetCustomerResponse>(customer);
         }
     }
 }
